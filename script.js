9943ef674b295a5d6391756159a4234916c1b3d4
dzmitrysopat <dzmitrysopat@gmail.com>
2023-12-05 15:47:56 +0300

    main

--- script.js
+++ script.js
@@ -19,3 +19,27 @@ function onEntry(entry) {
     // BackSpeed: 0.5,
     // loop:true
   })
+
+  var swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+});
